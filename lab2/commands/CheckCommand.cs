9478325bd594aa7cdf1cff574ed36ce6a22ef918
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 07:30:37 +0300

    [lab2]: check command

--- lab2/commands/CheckCommand.cs
+++ lab2/commands/CheckCommand.cs
@@ -8,6 +8,32 @@ public class CheckCommand : Command
 
     public override void Execute()
     {
-        throw new NotImplementedException();
+        try
+        {
+            var year = IOUtils.ReadPositiveInt("Input year: ");
+            if (year % 4 == 0)
+            {
+                Console.Write("The {0} ", year);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("is");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" leap.\n");
+            }
+            else
+            {
+                Console.Write("The {0} ", year);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("is not");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" leap.\n");
+            }
+        }
+        catch (InterruptionException) {
+            Console.WriteLine("Прервано пользователем.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
