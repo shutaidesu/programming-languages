bd4e1258d3daea9691c11f4d1f4b33df81a41dcd
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 04:54:57 +0300

    [lab1]: interfaces

---
+++ lab1/manipulators/MatrixManipulator.cs
@@ -0,0 +1,19 @@
+namespace Lab1;
+
+public class MatrixManipulator : IManipulator
+{
+    public MatrixManipulator(int rows, int columns)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void PrintResult()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ProcessData()
+    {
+        throw new NotImplementedException();
+    }
+}
