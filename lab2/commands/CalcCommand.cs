791bdb1bff71e8e472bd15e97c859c1456b38630
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 07:52:50 +0300

    [lab2]: calc command

--- lab2/commands/CalcCommand.cs
+++ lab2/commands/CalcCommand.cs
@@ -8,6 +8,32 @@ public class CalcCommand : Command
 
     public override void Execute()
     {
-        throw new NotImplementedException();
+        // Reading FROM
+        Console.Write("Input ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("From Date");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(".\n");
+        var dateFrom = IOUtils.ReadDate();
+
+        Console.Write("\n");
+
+        // Reading TO
+        Console.Write("Input ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("To Date");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(".\n");
+        var dateTo = IOUtils.ReadDate();
+
+        Console.Write("\n");
+
+        // Result
+        var differenceDays = (dateTo - dateFrom).TotalDays;
+        Console.Write("Between these dates ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("{0}", differenceDays);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(" days.\n");
     }
 }
