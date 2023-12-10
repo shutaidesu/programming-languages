949a1f2770074e51f7a2eb7bc58d847690c134ec
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 07:45:38 +0300

    [lab2]: day command

--- lab2/IOUtils.cs
+++ lab2/IOUtils.cs
@@ -22,6 +22,32 @@ public static class IOUtils
         }
     }
 
+    public static int ReadInt(string message, int min, int max)
+    {
+        while (true)
+        {
+            int result = ReadInt(message, min);
+            if (result <= max)
+            {
+                return result;
+            }
+            Console.WriteLine("Необходимо ввести число не больше чем {0}.", max);
+        }
+    }
+
+    public static int ReadInt(string message, int min)
+    {
+        while (true)
+        {
+            int result = ReadInt(message);
+            if (result >= min)
+            {
+                return result;
+            }
+            Console.WriteLine("Необходимо ввести число не меньше чем {0}.", min);
+        }
+    }
+
     public static int ReadInt(string message)
     {
         Console.Write(message);
@@ -53,6 +79,14 @@ public static class IOUtils
                 Console.Write(message);
             }
         }
+    }
+
+    public static DateTime ReadDate()
+    {
+        var year = ReadInt("Input year: ", 1, 9999);
+        var month = ReadInt("Input month: ", 1, 12);
+        var day = ReadInt("Input day: ", 1, DateTime.DaysInMonth(year, month));
+        return new DateTime(year, month, day);
     }
 
     internal static void PrintIntArray(int[] array)
