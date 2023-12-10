ae5b81ef7ec6f16b1f9f0aec6d136cf4fba1058f
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 11:12:49 +0300

    [lab3]: sqlite

--- lab3/data/HistoryEntity.cs
+++ lab3/data/HistoryEntity.cs
@@ -9,15 +9,39 @@ public class HistoryEntity
     private string message;
 
     public DateTime DateTime { get => dateTime; set => dateTime = value; }
-    
+
     public string Command { get => command; set => command = value; }
-    
-    public string Message { get => message; set => message = value; }
+
+    public string Message
+    {
+        get => message; set => message = value;
+    }
+
+    public HistoryEntity(Command<HistoryEntity> command, string message, DateTime dateTime)
+    {
+        Command = command.Name;
+        Message = message;
+        DateTime = dateTime;
+    }
+
+    public HistoryEntity(string command, string message, DateTime dateTime)
+    {
+        Command = command;
+        Message = message;
+        DateTime = dateTime;
+    }
 
     public HistoryEntity(Command<HistoryEntity> command, string message)
     {
-        this.Command = command.Name;
-        this.Message = message;
+        Command = command.Name;
+        Message = message;
+        DateTime = DateTime.Now;
+    }
+
+    public HistoryEntity(string command, string message)
+    {
+        Command = command;
+        Message = message;
         DateTime = DateTime.Now;
     }
 
