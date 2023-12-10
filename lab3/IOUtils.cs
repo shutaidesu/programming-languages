dad42084a89ef34155544cc98ec3fc5097e5fa7f
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 07:55:49 +0300

    [lab3]: fork from lab2

---
+++ lab3/IOUtils.cs
@@ -0,0 +1,108 @@
+﻿namespace Lab3;
+
+
+[Serializable]
+public class InterruptionException : Exception
+{
+}
+
+
+public static class IOUtils
+{
+    public static int ReadPositiveInt(string message)
+    {
+        while (true)
+        {
+            int result = ReadInt(message);
+            if (result > 0)
+            {
+                return result;
+            }
+            Console.WriteLine("Необходимо ввести положительное число.");
+        }
+    }
+
+    public static int ReadInt(string message, int min, int max)
+    {
+        while (true)
+        {
+            int result = ReadInt(message, min);
+            if (result <= max)
+            {
+                return result;
+            }
+            Console.WriteLine("Необходимо ввести число не больше чем {0}.", max);
+        }
+    }
+
+    public static int ReadInt(string message, int min)
+    {
+        while (true)
+        {
+            int result = ReadInt(message);
+            if (result >= min)
+            {
+                return result;
+            }
+            Console.WriteLine("Необходимо ввести число не меньше чем {0}.", min);
+        }
+    }
+
+    public static int ReadInt(string message)
+    {
+        Console.Write(message);
+        while (true)
+        {
+            var line = Console.ReadLine();
+
+
+            if (line == "exit")
+            {
+                throw new InterruptionException();
+            }
+
+            if (line == null)
+            {
+                Console.Write("Строка не может быть пустой. Введите целое число: ");
+                continue;
+            }
+
+            try
+            {
+                {
+                    return int.Parse(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Произошла ошибка при получении числа: " + e.Message);
+                Console.Write(message);
+            }
+        }
+    }
+
+    public static DateTime ReadDate()
+    {
+        var year = ReadInt("Input year: ", 1, 9999);
+        var month = ReadInt("Input month: ", 1, 12);
+        var day = ReadInt("Input day: ", 1, DateTime.DaysInMonth(year, month));
+        return new DateTime(year, month, day);
+    }
+
+    internal static void PrintIntArray(int[] array)
+    {
+        Console.WriteLine(string.Join(" ", array));
+    }
+
+    internal static void PrintIntMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write("{0,4} ", matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lab3/commands/CheckCommand.cs
@@ -0,0 +1,39 @@
+namespace Lab3;
+
+public class CheckCommand : Command
+{
+    public CheckCommand() : base("check", "check if a year is leap")
+    {
+    }
+
+    public override void Execute()
+    {
+        try
+        {
+            var year = IOUtils.ReadPositiveInt("Input year: ");
+            if (year % 4 == 0)
+            {
+                Console.Write("The {0} ", year);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("is");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" leap.\n");
+            }
+            else
+            {
+                Console.Write("The {0} ", year);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("is not");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" leap.\n");
+            }
+        }
+        catch (InterruptionException) {
+            Console.WriteLine("�������� �������������.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
