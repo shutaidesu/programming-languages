ee1c9aa0e7132268477a4738fbf86025e4f5a8ce
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 07:10:12 +0300

    [lab2]: application running

--- lab2/CommandManager.cs
+++ lab2/CommandManager.cs
@@ -1,4 +1,8 @@
-﻿namespace Lab2;
+﻿using System.Xml.Linq;
+
+namespace Lab2;
+
+public class NotExistCommandException : Exception { }
 
 internal class CommandManager
 {
@@ -16,6 +20,42 @@ internal class CommandManager
 
     public void Listen()
     {
-        throw new NotImplementedException();
+        while (Application.IsRunning)
+        {
+            var request = ReadRequest();
+            try
+            {
+                var command = HandleCommand(request);
+                command.Execute();
+            }
+            catch (NotExistCommandException)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Команда ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("{0}", request);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" не найдена.\n");
+            }
+        }
+    }
+
+    public string ReadRequest()
+    {
+        Console.Write(">>> ");
+        return Console.ReadLine().Trim();
+    }
+
+    public Command HandleCommand(string request)
+    {
+        foreach (var command in commands)
+        {
+            if (command.IsMatch(request))
+            {
+                return command;
+            }
+        }
+
+        throw new NotExistCommandException();
     }
 }
