aebbc9043128074229d6c4ff57f47f3956c6fe7a
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 05:58:10 +0300

    [lab1]: array manipulator

--- lab1/Constants.cs
+++ lab1/Constants.cs
@@ -4,6 +4,6 @@
 
 public static class Constants
 {
-    public static int MinRandomValue = -10;
-    public static int MaxRandomValue = 10;
+    public static int MinRandomValue = 0;
+    public static int MaxRandomValue = 3;
 }
