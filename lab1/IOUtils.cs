fb14f2972a9c87950962b4cbd29760fddb73837c
Polina Moskvitina <shutaidesu@mail.ru>
2023-12-10 05:07:49 +0300

    [lab1]: read int utils

--- lab1/IOUtils.cs
+++ lab1/IOUtils.cs
@@ -1,9 +1,57 @@
 ﻿namespace Lab1;
 
+
+[Serializable]
+public class InterruptionException : Exception
+{
+}
+
+
 public static class IOUtils
 {
-    public static int ReadInt(string input)
+    public static int ReadPositiveInt(string message)
     {
-        throw new NotImplementedException();
+        while (true)
+        {
+            int result = ReadInt(message);
+            if (result > 0)
+            {
+                return result;
+            }
+            Console.WriteLine("Необходимо ввести положительное число.");
+        }
+    }
+
+    public static int ReadInt(string message)
+    {
+        Console.Write(message);
+        while (true)
+        {
+            var line = Console.ReadLine();
+
+
+            if (line == "exit")
+            {
+                throw new InterruptionException();
+            }
+
+            if (line == null)
+            {
+                Console.Write("Строка не может быть пустой. Введите целое число: ");
+                continue;
+            }
+
+            try
+            {
+                {
+                    return int.Parse(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Произошла ошибка при получении числа: " + e.Message);
+                Console.Write(message);
+            }
+        }
     }
 }
