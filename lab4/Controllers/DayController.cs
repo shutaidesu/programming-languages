b26a8d6ab00cf0af02f5fe6ae9a8cfcd700dc2c2
Polina Moskvitina <shutaidesu@mail.ru>
2024-01-09 12:27:24 +0300

    [lab4] day controller

---
+++ lab4/Controllers/DayController.cs
@@ -0,0 +1,29 @@
+using lab4.Data;
+using lab4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab4.Controllers;
+
+/// <summary>
+/// Return weekday of the specific date.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class DayController : ControllerBase
+{
+    private readonly DatesContext _context;
+
+    public DayController(DatesContext context, ILogger<DayController> logger)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public string Get(DateTime date)
+    {
+        var dayOfWeek = date.DayOfWeek;
+        string message = string.Format("{0} is {1}.", date.ToString("dd.MM.yyyy"), dayOfWeek.ToString());
+        _context.HistoryEntities.Add(new HistoryEntity(Command.Day, message));
+        return dayOfWeek.ToString();
+    }
+}
